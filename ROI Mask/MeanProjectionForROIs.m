17e679e19e948c27d0b92963029ebaca499159d2
Lockhart <jsl5865@psu.edu>
2025-08-07 17:20:47 -0400

    Created a function to create a projected mean intensity (similar to imageJ), but specifically for defining ROIs. A more advanced version of project mean intensity needs to be created to better average frames together, but this is a simple version that takes any stack and averages together the entire z dimension. The output of this function is the creation of a figure window with its figure handle

---
+++ ROI Mask/MeanProjectionForROIs.m
@@ -0,0 +1,13 @@
+function ax = MeanProjectionForROIs(Stack)
+    % Show mean projection of image stack and return axes handle
+    figure('Name', 'Draw ROI(s)')
+    imagesc(mean(Stack, 3));
+
+    axis image;
+    colormap gray;
+    xlabel('Pixels');
+    ylabel('Pixels');
+    title('Press Enter When Done');
+    
+    ax = gca;
+end
