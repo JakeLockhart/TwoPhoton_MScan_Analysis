fecf209baa27e1b7f80cabb6ee9e8007416808cf
Lockhart <jsl5865@psu.edu>
2025-08-07 17:18:57 -0400

    Created a function to draw regions of interest on a single image. The ROI shape is determined by the user and currently supports rectangles, circles, lines, polygons, and freehands. In the future, create more unique ROI regions for cases such as full width at half maximum analysis. This currently works for any valid figure window by using the figurehandle (ax).

---
+++ ROI Mask/DrawROI.m
@@ -0,0 +1,25 @@
+function ROI = DrawROI(ax, ROI_Shape)
+    arguments
+        ax (1,1) matlab.graphics.axis.Axes
+        ROI_Shape char {mustBeMember(ROI_Shape, {'Rectangle', 'Circle', 'Line', 'Polygon', 'Freehand'})} = 'Rectangle'
+    end
+
+    title(ax, sprintf('Draw a %s ROI (double-click or press Enter when done)', ROI_Shape));
+
+    switch ROI_Shape
+        case 'Rectangle'
+            ROI = drawrectangle(ax);
+        case 'Circle'
+            ROI = drawcircle(ax);
+        case 'Line'
+            ROI = drawline(ax);
+        case 'Polygon'
+            ROI = drawpolygon(ax);
+        case 'Freehand'
+            ROI = drawfreehand(ax);
+        otherwise
+            error('ROI shape not available')
+    end
+
+    wait(ROI);
+end
