27e551e90bd1b8295f7f6febaf2d1bc413285cbf
jake <jakelockhart65@gmail.com>
2025-08-08 12:47:09 -0400

    Updated documentation

--- ImageJ Functions/Deinterleave.m
+++ ImageJ Functions/Deinterleave.m
@@ -1,4 +1,31 @@
 function DeinterleavedStack = Deinterleave(Stack, Channels)
+    % <Documentation>
+        % Deinterleave()
+        %   Deinterleave an image stack into a user defined number of channels, similarly to ImageJ(FIJI).
+        %   Created by: jsl5865
+        %   
+        % Syntax:
+        %   DeinterleavedStack = Deinterleave(Stack, Channels)
+        %
+        % Description:
+        %   This function splits a multi-frame image stack into separate stacks, based on the number of 
+        %       channels specified. The input stack is expected to have frames interleaved. If the total
+        %       number of frames is not divisible by the number of channels, the excess frames at the end
+        %       of the original stack are discarded.
+        %   Assumed that frames are interleaved based on channel. I.E.:
+        %       Channels: 2
+        %       Frame 1 = Channel 1 , Frame 2 = Channel 2, Frame 3 = Channel 1 ...
+        %   
+        % Input:
+        %   Stack       - A numerical 3D array representing an image stack, with dimensions 
+        %                 Rows x Columns x Frames. 
+        %   Channels    - Integer specifying the total number of channels to separate the stack into.
+        %   
+        % Output:
+        %   DeinterleavedStack  - A struct with fields 'Channel_1', 'Channel_2', ..., 'Channel_N', each 
+        %                         containing a 3D array of the deinterleaved frames for that channel.
+    % <End Documentation>
+    
     fprintf('Deinterleaving MDF file into %d channels...  \n', Channels)
     [~, ~, TotalFrames] = size(Stack);
     ExcessFrames = mod(TotalFrames, Channels); 
