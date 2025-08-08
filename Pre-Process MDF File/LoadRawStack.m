9ea224361b4d33366f202513d6f4745ed0c05f31
Lockhart <jsl5865@psu.edu>
2025-08-08 13:09:41 -0400

    Updated documentation

--- Pre-Process MDF File/LoadRawStack.m
+++ Pre-Process MDF File/LoadRawStack.m
@@ -1,4 +1,34 @@
 function RawStack = LoadRawStack(obj)
+    % <Documentation>
+        % LoadRawStack()
+        %   Load .MDF file into workspace as a 3D numerical array (int16) in batches to improve memory performance.
+        %   Created by: jsl5865
+        %   
+        % Syntax:
+        %   RawStack = LoadRawStack(obj)
+        %
+        % Description:
+        %   This function reads a .MDF file's multi-frame image stack using batch processing to improve efficiency
+        %       and reduce memory load. 
+        %   File metadata is used to preallocated numerical array dimensions. 
+        %   Batch loading segments the .MDF file into sections of 1000 frames. These sections are stored as cells 
+        %       and eventually concatentated into a single 3D numerical array once all image frames have been 
+        %       read and saved. The time to load each batch is displayed.
+        %   Interal helper function ('LoadBatchStack()') handles batch processing.
+        %   Processing 'bottleneck':
+        %       MCS.ReadFrame() appears to be the rate limiting step of this process. If reading the frames from
+        %       the .MDF file can be done faster, this function will run more efficiently.
+        %
+        % Input:
+        %   obj - Object containing:
+        %           obj.File.MCS      : actxserver() necessary to read .MDF file frames.
+        %           obj.File.MetaData : File metadata necessary to preallocate array dimensions.
+        %
+        % Output:
+        %   RawStack  - Numerical 3D array (Rows x Columns x Frames) containing all frames
+        %               from the MCS file, read in sequential order.
+    % <End Documentation>
+
     fprintf('Creating raw stack...  \n')
     MCS = obj.File.MCS;
     MetaData = obj.File.MetaData;
