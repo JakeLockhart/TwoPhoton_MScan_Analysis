b95be49f0673fd36aa71b64d1827d9645cd75232
jake <jakelockhart65@gmail.com>
2025-08-09 14:20:02 -0400

    Updated documentation

--- ImageJ Functions/MeanProjection.m
+++ ImageJ Functions/MeanProjection.m
@@ -1,5 +1,26 @@
 function ax = MeanProjection(Stack)
-    % Show mean projection of image stack and return axes handle
+    % <Documentation>
+        % MeanProjection()
+        %   Average all frames of an image stack into a single frame, similarly to ImageJ(FIJI).
+        %   Created by: jsl5865
+        %
+        % Syntax:
+        %   ax = MeanProjection(Stack)
+        %
+        % Description:
+        %   This fucntion creates a mean projection of a 3D image stack by averageing the pixel
+        %       values across the third dimension (frame). 
+        %   Once an averaged 2D image frame has been created, a grayscale image is created in a 
+        %       figure window.
+        %   
+        % Input:
+        %   Stack - A numerical 3D array representing an image stack, with dimensions 
+        %           Rows x Columns x Frames.
+        %
+        % Output:
+        %   ax - Returns the handles for the figure's axis object
+        %
+    % <End Documentation>
     figure('Name', 'Draw ROI(s)')
     imagesc(mean(Stack, 3));
 
@@ -7,7 +28,6 @@ function ax = MeanProjection(Stack)
     colormap gray;
     xlabel('Pixels');
     ylabel('Pixels');
-    title('Press Enter When Done');
     
     ax = gca;
 end
