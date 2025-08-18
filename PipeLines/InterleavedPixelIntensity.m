5b279b9a723bc57ffb1d9d80f97b8fb03e51fa10
Lockhart <jsl5865@psu.edu>
2025-08-17 23:14:37 -0400

    Updated documentation

--- PipeLines/InterleavedPixelIntensity.m
+++ PipeLines/InterleavedPixelIntensity.m
@@ -1,16 +1,28 @@
 function [obj, PixelIntensity] = InterleavedPixelIntensity(obj)
     % <Documentation>
         % InterleavedPixelIntensity()
-        %   
+        %   Calculates the mean pixel intensity of an ROI from an interleaved image stack.
         %   Created by: jsl5865
         %   
         % Syntax:
-        %   
+        %   [obj, PixelIntensity] = InterleavedPixelIntensity(obj)
+        %
         % Description:
-        %   
+        %   This function processes interleaved image stacks contained within an object, applies 
+        %       pre-processing parameters, deletes unwanted frames, deinterleaves channels, and 
+        %       allows the user to draw a region of interest (ROI) on the final deinterleaved 
+        %       stack. The stacks are then cropped to the ROI, and mean pixel intensities along 
+        %       the Z-axis are computed for each stack.
+        %
         % Input:
-        %   
+        %   obj - MScan_Analysis object containing raw image stack from a .MDF file
+        %
         % Output:
+        %   obj             - Updated MScan_Analysis object contianing 'Fixed' and 'Deinterleaved' 
+        %                     image stacks along with ROI masks.
+        %   PixelIntensity  - A structure containing fields from each imaging channel with numerical
+        %                     arrays containing the mean pixel intensity of an ROI across an image 
+        %                     stack's frames
         %   
     % <End Documentation>
 
