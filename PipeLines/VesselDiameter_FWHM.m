ab8fa89eac253e5d17833deeeb5ca635a3394e8d
Lockhart <jsl5865@psu.edu>
2025-09-04 20:40:30 -0400

    Creating pipeline to measure vessel diameter given a set of ROIs. The goal is to make a for loop where each loop The FWHM across each ROI is made, and then iterates to the next image frame.

---
+++ PipeLines/VesselDiameter_FWHM.m
@@ -0,0 +1,24 @@
+function [] = VesselDiameter_FWHM(Stack, MicronsPerPixel)
+    % <Documentation>
+        % VesselDiameter_FWHM()
+        %   
+        %   Created by: jsl5865
+        %   
+        % Syntax:
+        %   
+        % Description:
+        %   
+        % Input:
+        %   
+        % Output:
+        %   
+    % <End Documentation>
+
+    fprintf('Calculating the vessel diameter using the Full-Width at Half-Maximum technique.\n\n')
+
+    Fields = fieldnames(obj.Stack.Raw) 
+
+
+
+
+end
