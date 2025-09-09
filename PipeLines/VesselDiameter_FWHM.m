c1d94f47de665940b72946831d2fee48336a5f8f
Lockhart <jsl5865@psu.edu>
2025-09-09 14:44:53 -0400

    Created a loop to calculate the FWHM of a vessel's diameter based on the results of the TileStack_DrawROI(). This loops through all ROIs from a single panel, calculating the ROI AdjustedImprofile() and subsequently the CalculateFWHM() during one frame. This removes the need of iterating through the same image stack multiple times.

--- PipeLines/VesselDiameter_FWHM.m
+++ PipeLines/VesselDiameter_FWHM.m
@@ -1,4 +1,4 @@
-function [] = VesselDiameter_FWHM(Stack, MicronsPerPixel)
+function [FWHM, Edge1, Edge2, ROIProfile] = VesselDiameter_FWHM(Stack, LineROI, LineWidth, MicronsPerPixel)
     % <Documentation>
         % VesselDiameter_FWHM()
         %   
@@ -14,11 +14,26 @@ function [] = VesselDiameter_FWHM(Stack, MicronsPerPixel)
         %   
     % <End Documentation>
 
-    fprintf('Calculating the vessel diameter using the Full-Width at Half-Maximum technique.\n\n')
+    fprintf('Calculating the vessel diameter using the Full-Width at Half-Maximum technique.\n')
 
-    Fields = fieldnames(obj.Stack.Raw) 
-
-
-
+    Frames = size(Stack,3);
+    ROIs = length(LineROI);
+    SamplesOnROI = 1000;
+    ROIProfile = NaN(SamplesOnROI, Frames, ROIs);
+    
+    ROIProfilePosition = linspace(0, SamplesOnROI-1, SamplesOnROI) * MicronsPerPixel;
+    FWHM = NaN(Frames,ROIs);
+    Edge1 = NaN(Frames,ROIs);
+    Edge2 = NaN(Frames,ROIs);
+    
+    Timer = tic;
+    for i = 1:Frames
+        for j = 1:ROIs
+            ROIProfile(:,i,j) = AdjustedImprofile(Stack(:,:,i), LineROI{j}, LineWidth{j});
+            [FWHM(i,j), Edge1(i,j), Edge2(i,j)] = CalculateFWHM(ROIProfilePosition, ROIProfile(:,i,j));
+        end
+    end
+    
+    fprintf('Completed calculating FWHM (%.2fs) ✓\n', toc(Timer))
 
 end
