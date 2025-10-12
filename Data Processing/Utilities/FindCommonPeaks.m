bc9d3d9ead943138563c7f1027258d75e56b72c3
Lockhart <jsl5865@psu.edu>
2025-10-12 15:15:20 -0400

    This function finds the common peaks across multiple data sets. The idea is to average all the data sets together and then use findpeaks() to determine the locations of the 'true' peaks that should occur in each of the data sets. The peak locations should then correspond to 'real' peaks that occur in each data set.

---
+++ Data Processing/Utilities/FindCommonPeaks.m
@@ -0,0 +1,30 @@
+function [PeakLocations, PeakWidths] =FindCommonPeaks(VectorArray, TimeAxis, MinProminence)
+    % <Documentation>
+        % FindCommonPeaks()
+        %   
+        %   Created by: jsl5865
+        %   
+        % Syntax:
+        %   
+        % Description:
+        %   
+        % Input:
+        %   
+        % Output:
+        %   
+    % <End Documentation>
+
+    arguments
+        VectorArray
+        TimeAxis
+        MinProminence (1,1) double {mustBeNumeric, mustBePositive} = 0.2
+    end
+
+    AverageDataSet = mean(VectorArray, 2);
+    [~, PeakLocations, PeakWidths] = findpeaks(AverageDataSet, TimeAxis, ...
+                               "MinPeakProminence", MinProminence, ...
+                               "WidthReference", "halfheight" ...
+                              );
+    PeakWidths = PeakWidths*2;
+
+end
