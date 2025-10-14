7fbc29383676ce0de159d2e3c278321f95a5221b
Lockhart <jsl5865@psu.edu>
2025-10-14 10:51:14 -0400

    Created a function to plot the sliding cross correlation of the vasoactivity for a vessel

---
+++ Data Processing/Vessel Diameter/Plot_SlidingCrossCorrelation.m
@@ -0,0 +1,33 @@
+function Plot_SlidingCrossCorrelation(VesselDiameter, CorrelationMatrix, FPS)
+    % <Documentation>
+        % Plot_SlidingCrossCorrelation()
+        %   
+        %   Created by: jsl5865
+        %   
+        % Syntax:
+        %   
+        % Description:
+        %   
+        % Input:
+        %   
+        % Output:
+        %   
+    % <End Documentation>
+
+    Lag = VesselDiameter.XCorr.Lag / FPS;
+    FrameAxis = VesselDiameter.XCorr.FrameAxis / FPS;
+
+    figure
+    imagesc(FrameAxis, Lag, CorrelationMatrix);
+    
+    title('Sliding Cross Correlation of Vasoactivity')
+    xlabel('Time [s]')
+    ylabel('Lag [s]')
+    axis xy
+    colormap turbo
+
+    cb = colorbar;
+    cb.Label.String = 'Correlation Coefficient';
+    clim([-1, 1]);
+    
+end
